7406d56b7e788014ff4e68583b557772931dfbed
Denys Prokhorin <prokhorid@gmail.com>
2020-01-07 19:15:05 +0200

    minor issues

--- src/main/java/ua/friends/telegram/bot/command/impl/GlashatayCommand.java
+++ src/main/java/ua/friends/telegram/bot/command/impl/GlashatayCommand.java
@@ -16,7 +16,7 @@ public class GlashatayCommand implements Command {
     }
 
     private String buildMessage(Update update) {
-        String text = update.getMessage().getText().split(" ")[1];
+        String text = update.getMessage().getText().split(" ",2)[1];
         StringBuilder sb = new StringBuilder();
         sb.append(findName(update));
         sb.append(" ");
