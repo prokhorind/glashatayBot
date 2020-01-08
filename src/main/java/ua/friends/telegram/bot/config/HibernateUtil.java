a93d6b9beb4257983668fc54d8beb961c86b8b75
Denys Prokhorin <prokhorid@gmail.com>
2020-01-08 18:38:27 +0200

    add hibernate util

---
+++ src/main/java/ua/friends/telegram/bot/config/HibernateUtil.java
@@ -0,0 +1,42 @@
+package ua.friends.telegram.bot.config;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+import ua.friends.telegram.bot.entity.Chat;
+import ua.friends.telegram.bot.entity.User;
+
+import java.util.Properties;
+
+public class HibernateUtil {
+    private static SessionFactory sessionFactory;
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+                // Hibernate settings equivalent to hibernate.cfg.xml's properties
+                Properties settings = new Properties();
+                settings.put(Environment.DRIVER, "org.postgresql.Driver");
+                settings.put(Environment.URL, System.getenv("DB_URL"));
+                settings.put(Environment.USER, System.getenv("DB_USER"));
+                settings.put(Environment.PASS, System.getenv("DB_PASS"));
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.PostgreSQL94Dialect");
+                settings.put(Environment.SHOW_SQL, "true");
+                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+                settings.put(Environment.HBM2DDL_AUTO, "update");
+                configuration.setProperties(settings);
+                configuration.addAnnotatedClass(User.class);
+                configuration.addAnnotatedClass(Chat.class);
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+        return sessionFactory;
+    }
+}
