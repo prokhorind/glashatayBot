9646d5fdb065b839df5b5cf0117d25826b77bd45
Denys Prokhorin <prokhorid@gmail.com>
2020-03-20 17:28:01 +0200

    parse heroku string db connection

--- src/main/java/ua/friends/telegram/bot/config/HibernateUtil.java
+++ src/main/java/ua/friends/telegram/bot/config/HibernateUtil.java
@@ -7,20 +7,28 @@ import org.hibernate.cfg.Environment;
 import org.hibernate.service.ServiceRegistry;
 import ua.friends.telegram.bot.entity.*;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 public class HibernateUtil {
+    public static final String LOGIN = "login";
+    public static final String PASS = "pass";
+    public static final String URL = "url";
     private static SessionFactory sessionFactory;
+
     public static SessionFactory getSessionFactory() {
         if (sessionFactory == null) {
             try {
+
+                Map map = parseUrl(System.getenv("DATABASE_URL"));
                 Configuration configuration = new Configuration();
                 // Hibernate settings equivalent to hibernate.cfg.xml's properties
                 Properties settings = new Properties();
                 settings.put(Environment.DRIVER, "org.postgresql.Driver");
-                settings.put(Environment.URL, System.getenv("DB_URL"));
-                settings.put(Environment.USER, System.getenv("DB_USER"));
-                settings.put(Environment.PASS, System.getenv("DB_PASS"));
+                settings.put(Environment.URL, map.get(URL));
+                settings.put(Environment.USER, map.get(LOGIN));
+                settings.put(Environment.PASS, map.get(PASS));
                 settings.put(Environment.DIALECT, "org.hibernate.dialect.PostgreSQL94Dialect");
                 settings.put(Environment.SHOW_SQL, "false");
                 settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
@@ -43,5 +51,18 @@ public class HibernateUtil {
 
         }
         return sessionFactory;
+    }
+
+    private static Map<String, String> parseUrl(String url) {
+        Map<String, String> map = new HashMap<>();
+        String[] prefixAndConnectionUrl = url.split("://");
+        String noPrefixConnectionUrl = prefixAndConnectionUrl[1];
+        String login = noPrefixConnectionUrl.split(":")[0];
+        String pass = noPrefixConnectionUrl.split(":")[1].split("@")[0];
+        String urlDb = String.format("%s%s", "jdbc:postgresql://", noPrefixConnectionUrl.split(":", 2)[1].split("@")[1]);
+        map.put(LOGIN, login);
+        map.put(PASS, pass);
+        map.put(URL, urlDb);
+        return map;
     }
 }
