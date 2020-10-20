8c23a9ebc165bba14206511bf994627ddf7baebc
prokhorind <prokhorind@gmail.com>
2020-10-20 18:30:47 +0300

    fix null in gay reg command

--- src/main/java/ua/friends/telegram/bot/command/impl/GayRegCommand.java
+++ src/main/java/ua/friends/telegram/bot/command/impl/GayRegCommand.java
@@ -1,7 +1,13 @@
 package ua.friends.telegram.bot.command.impl;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+
 import ua.friends.telegram.bot.command.Command;
 import ua.friends.telegram.bot.command.MessageUtils;
 import ua.friends.telegram.bot.entity.Chat;
@@ -10,16 +16,18 @@ import ua.friends.telegram.bot.service.ChatService;
 import ua.friends.telegram.bot.service.GayGameService;
 import ua.friends.telegram.bot.service.UserService;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.logging.Logger;
-import java.util.stream.Collectors;
+import static ua.friends.telegram.bot.utils.TelegramNameUtils.findName;
 
 public class GayRegCommand implements Command {
-    private static final String MESSAGE = "уже зарегистрирован";
+
+    private static final String MESSAGE = "не зарегистрирован";
+
     private static final String SUCCESS_MESSAGE = "успешно зарегистрирован";
+
     private UserService userService = new UserService();
+
     private ChatService chatService = new ChatService();
+
     private GayGameService gayGameService = new GayGameService();
 
     private Logger logger = Logger.getLogger(GayRegCommand.class.getName());
@@ -31,11 +39,14 @@ public class GayRegCommand implements Command {
         int tgId = update.getMessage().getFrom().getId();
         User user = userService.find(tgId, chatId).get();
         Chat chat = chatService.find(chatId).get();
+        String userName = findName(user, true);
 
         if (user.getGayChats().stream().map(a -> a.getChatId()).collect(Collectors.toSet()).contains(chatId)) {
-            return Collections.singletonList(MessageUtils.generateMessage(update.getMessage().getChatId(), String.format("%s %s", user.getLogin(), MESSAGE)));
+            return Collections
+                .singletonList(MessageUtils.generateMessage(update.getMessage().getChatId(), String.format("%s %s", userName, MESSAGE)));
         }
         gayGameService.reg(user, chat);
-        return Collections.singletonList(MessageUtils.generateMessage(update.getMessage().getChatId(), String.format("%s %s", user.getLogin(), SUCCESS_MESSAGE)));
+        return Collections.singletonList(
+            MessageUtils.generateMessage(update.getMessage().getChatId(), String.format("%s %s", userName, SUCCESS_MESSAGE)));
     }
 }
