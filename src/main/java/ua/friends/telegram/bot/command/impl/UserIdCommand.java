42f8e3a2dd85b30fa6470d84611768fe3b3c3d0f
prokhorind <prokhorind@gmail.com>
2020-12-04 21:13:46 +0200

    Add all users tgId view

--- src/main/java/ua/friends/telegram/bot/command/impl/UserIdCommand.java
+++ src/main/java/ua/friends/telegram/bot/command/impl/UserIdCommand.java
@@ -1,18 +1,23 @@
 package ua.friends.telegram.bot.command.impl;
 
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Update;
-import ua.friends.telegram.bot.command.Command;
-import ua.friends.telegram.bot.command.MessageUtils;
-import ua.friends.telegram.bot.entity.User;
-import ua.friends.telegram.bot.service.UserService;
-import ua.friends.telegram.bot.service.UserServiceImpl;
-
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.inject.Inject;
+
+import org.jetbrains.annotations.NotNull;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+import ua.friends.telegram.bot.command.Command;
+import ua.friends.telegram.bot.command.MessageUtils;
+import ua.friends.telegram.bot.entity.Chat;
+import ua.friends.telegram.bot.entity.User;
+import ua.friends.telegram.bot.service.ChatService;
+import ua.friends.telegram.bot.service.UserService;
+import ua.friends.telegram.bot.utils.TelegramNameUtils;
 
 public class UserIdCommand implements Command {
 
@@ -22,6 +27,9 @@ public class UserIdCommand implements Command {
     @Inject
     private UserService userService;
 
+    @Inject
+    private ChatService chatService;
+
     @Override
     public List<SendMessage> executeCommand(Update update) {
         String[] params = update.getMessage().getText().split(" ", 2);
@@ -30,12 +38,34 @@ public class UserIdCommand implements Command {
         if (params.length == 1 || params[1] == null || params[1].isEmpty()) {
             return Collections.singletonList(MessageUtils.generateMessage(chatId, SET_NAME));
         }
+
+        if (params.length == 2 || "ALL".equalsIgnoreCase(params[1])) {
+            return getAllUsersTgIdsInChat(chatId);
+        }
+
         String to = params[1].replaceAll("@", "");
         return Collections.singletonList(MessageUtils.generateMessage(chatId, createMessage(to, chatId)));
     }
 
-    private String createMessage(String to, long chatID) {
-        Optional<User> oUser = userService.find(to, chatID);
+    @NotNull
+    private List<SendMessage> getAllUsersTgIdsInChat(long chatId) {
+        StringBuilder sb = new StringBuilder();
+        Chat chat = chatService.find(chatId).get();
+        AtomicInteger playerNum = new AtomicInteger(0);
+        chat.getUsers().forEach(g -> buildMessage(sb, g, playerNum));
+        return Collections.singletonList(MessageUtils.generateMessage(chatId, sb.toString(),"HTML"));
+    }
+
+    private void buildMessage(StringBuilder sb, User user, AtomicInteger playerNum) {
+        sb.append(String.format("%s%d%s%s ","<strong>",playerNum.incrementAndGet(),".","</strong>"));
+        sb.append(TelegramNameUtils.findName(user, false));
+        sb.append(" : ");
+        sb.append(user.getTgId());
+        sb.append("\n");
+    }
+
+    private String createMessage(String login, long chatID) {
+        Optional<User> oUser = userService.find(login, chatID);
         if (oUser.isPresent()) {
             return oUser.get().getTgId().toString();
         } else {
