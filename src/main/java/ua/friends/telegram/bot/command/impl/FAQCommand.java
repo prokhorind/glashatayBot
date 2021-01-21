d500d1da0717cbacdfeb5514a352426d16b7eda1
prokhorind <prokhorind@gmail.com>
2021-01-22 00:07:37 +0200

    change message format

--- src/main/java/ua/friends/telegram/bot/command/impl/FAQCommand.java
+++ src/main/java/ua/friends/telegram/bot/command/impl/FAQCommand.java
@@ -2,11 +2,13 @@ package ua.friends.telegram.bot.command.impl;
 
 import java.io.FileInputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 import org.apache.commons.io.IOUtils;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 import ua.friends.telegram.bot.command.Command;
@@ -14,6 +16,7 @@ import ua.friends.telegram.bot.command.MessageUtils;
 import ua.friends.telegram.bot.utils.AdminUtils;
 
 public class FAQCommand implements Command {
+
     @Override
     public List<BotApiMethod> executeCommand(Update update) {
         String[] command = update.getMessage().getText().split(" ", 2);
@@ -23,13 +26,15 @@ public class FAQCommand implements Command {
             try {
                 fis = new FileInputStream("src/main/resources/faq.txt");
                 String data = IOUtils.toString(fis, StandardCharsets.UTF_8);
-                return Collections.singletonList(MessageUtils.generateMessage(chatId,data));
+                return Collections.singletonList(MessageUtils.generateMessage(chatId, data));
             } catch (Exception e) {
-               return Collections.singletonList(MessageUtils.generateMessage(chatId,"нет доступа к FAQ файлу"));
+                return Collections.singletonList(MessageUtils.generateMessage(chatId, "нет доступа к FAQ файлу"));
             }
         }
-        return Collections.singletonList(MessageUtils.generateMessage(AdminUtils.getChatAdminId(),String.format("%s:%s", chatId,command[1])));
+        SendMessage toAdmin = MessageUtils.generateMessage(AdminUtils.getChatAdminId(),
+            String.format("s%::::::::%s:\n%s", update.getMessage().getFrom().toString(), chatId, command[1]));
+        SendMessage toUser = MessageUtils.generateMessage(chatId, "Доставлено");
+        return Arrays.asList(toAdmin, toUser);
     }
-
 
 }
