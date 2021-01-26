c6275907e182da060f042175ef3723ccff6d6582
prokhorind <prokhorind@gmail.com>
2021-01-26 20:14:58 +0200

    update command link

--- src/main/java/ua/friends/telegram/bot/command/impl/AdminResponseCommand.java
+++ src/main/java/ua/friends/telegram/bot/command/impl/AdminResponseCommand.java
@@ -15,11 +15,12 @@ import ua.friends.telegram.bot.utils.AdminUtils;
 
 public class AdminResponseCommand implements Command {
 
-    public static final int RESPONSE_TO_SPECIFIC_CHAT = 3;
+    public static final int RESPONSE_TO_SPECIFIC_CHAT = 2;
 
     @Override
     public List<BotApiMethod> executeCommand(Update update) {
-        String[] command = update.getMessage().getText().split("&", 3);
+        String text = update.getMessage().getText().trim().split(" ", 2)[1];
+        String[] arguments = text.split("&");
         long fromChatId = update.getMessage().getChatId();
         User user = update.getMessage().getFrom();
 
@@ -28,16 +29,16 @@ public class AdminResponseCommand implements Command {
         }
         List<BotApiMethod> messages = new ArrayList<BotApiMethod>();
 
-        if (command.length == RESPONSE_TO_SPECIFIC_CHAT) {
-            return generateMessageResponseForSpecificChat(command, messages);
+        if (arguments.length == RESPONSE_TO_SPECIFIC_CHAT) {
+            return generateMessageResponseForSpecificChat(arguments, messages);
         }
         return Collections.singletonList(MessageUtils.generateMessage(fromChatId, "Ответ не отправлен"));
     }
 
     @NotNull
     private List<BotApiMethod> generateMessageResponseForSpecificChat(String[] command, List<BotApiMethod> messages) {
-        long chatId = Long.parseLong(command[1]);
-        String text = command[2];
+        long chatId = Long.parseLong(command[0]);
+        String text = command[1];
         messages.add(MessageUtils.generateMessage(chatId, "From admin:"));
         messages.add(MessageUtils.generateMessage(chatId, text));
         return messages;
